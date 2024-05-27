bef69da67f1c5660dc1db27d6d4be04d4036f83e
haidev99bk <haidev99bk@gmail.com>
2024-05-28 01:19:26 +0700

    update

---
+++ bin/ebSetUpEnv.sh
@@ -0,0 +1,1 @@
+eb setenv  POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=myPassword POSTGRES_HOST=database-2.crgaaaxfzzoq.us-east-1.rds.amazonaws.com POSTGRES_DB=postgres AWS_BUCKET=arn:aws:s3:::myawsbucket-hainv46 AWS_REGION=us-east-1 AWS_PROFILE=default JWT_SECRET=mysecretstring URL=http://udagram-api-dev5.us-east-1.elasticbeanstalk.com
